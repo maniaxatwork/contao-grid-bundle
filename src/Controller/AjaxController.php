eac8ad3d67cb3ec8cd354ad920e2744ec3891f82
maniax-at-work <40065105+maniax-at-work@users.noreply.github.com>
2024-04-23 11:00:23 +0200

    Initial commit

--- src/Controller/AjaxController.php
+++ src/Controller/AjaxController.php
@@ -4,37 +4,39 @@ declare(strict_types=1);
 
 namespace ManiaxAtWork\ContaoGridBundle\Controller;
 
+use Contao\System;
+use Contao\StringUtil;
+use Doctrine\DBAL\Connection;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpKernel\Attribute\AsController;
-use Symfony\Component\Security\Core\Security;
 use Terminal42\ServiceAnnotationBundle\Annotation\ServiceTag;
-use Contao\StringUtil;
 
 #[Route('/ajaxcolsave', name: AjaxController::class, defaults: ['_scope' => 'backend', '_token_check' => true])]
 #[AsController]
 class AjaxController
 {
     private $security;
+    private $db;
 
     public function __construct(Security $security)
     {
         $this->security = $security;
+        $this->db = System::getContainer()->get('doctrine.dbal.foo_connection');
     }
 
     public function __invoke(Request $request): Response
     {
-      //  $token = $this->security->getToken();
-
-       // $itemId = $request->request->get('id');
-       // $classes = $request->request->get('class');
-        $oldClass = $request->request->get('oldclass');
-       // $objResult = \Database::getInstance()->prepare("SELECT grid_columns FROM tl_content WHERE id=?")->execute($itemId);
-       // $objResult = \Database::getInstance()->prepare("UPDATE tl_content SET grid_columns=? WHERE id=?")->execute($classes, $itemId);
+       $itemId = $request->query->get('id');
+       $classes = $request->query->get('class');
+        $oldClass = $request->query->get('oldclass');
+       $objResult = $this->db->prepare("SELECT grid_columns FROM tl_content WHERE id=?")->execute($itemId);
+       //$objResult = \Database::getInstance()->prepare("UPDATE tl_content SET grid_columns=? WHERE id=?")->execute($classes, $itemId);
 
        //return new Response(StringUtil::deserialize($objResult));
 
-       return new Response($oldClass);
+       return new Response(StringUtil::deserialize($objResult));
     }
 }
