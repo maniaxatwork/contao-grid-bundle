f763a8682aeeeceaaf3407e47b12da688bf5338e
maniax-at-work <40065105+maniax-at-work@users.noreply.github.com>
2024-04-24 16:38:58 +0200

    Initial commit

--- contao/config/config.php
+++ contao/config/config.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of maniaxatwork/contao-grid-bundle.
+ *
+ * (c) maniax-at-work.de <https://www.maniax-at-work.de>
+ *
+ * @license MIT
+ */
+
 $GLOBALS['TL_WRAPPERS']['start'][] = 'rowStart';
 $GLOBALS['TL_WRAPPERS']['stop'][] = 'rowEnd';
 $GLOBALS['TL_WRAPPERS']['start'][] = 'colStart';
