77cfe6b11fec6b657b3c8d1d1bd3300e2e6dfdc8
maniax-at-work <40065105+maniax-at-work@users.noreply.github.com>
2024-04-24 15:42:00 +0200

    Initial commit

--- src/EventListener/DataContainer/RegisterFieldsInPaletteListener.php
+++ src/EventListener/DataContainer/RegisterFieldsInPaletteListener.php
@@ -15,7 +15,8 @@ final class RegisterFieldsInPaletteListener
     public function onLoadContentCallback(): void
     {
         foreach ($GLOBALS['TL_DCA']['tl_content']['palettes'] as $k => $palette) {
-            if (!\is_array($palette) && str_contains($palette, 'cssID')) {
+
+            if (!\is_array($palette) && str_contains($palette, 'cssID') && str_contains($k, 'colStart')) {
                 $GLOBALS['TL_DCA']['tl_content']['palettes'][$k] = str_replace(
                     '{invisible_legend',
                     '{grid_legend},grid_columns,grid_options;{invisible_legend',
