1cffe3e2f155ac32bb01ecb76183a772cf6261b3
maniax-at-work <40065105+maniax-at-work@users.noreply.github.com>
2024-04-24 14:33:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+![Alt text](docs/logo.png?raw=true "logo")
+
+
+# Welcome to contao-grid-bundle
+This bundle is still under construction.
--- src/Controller/ContentElement/RowStartController.php
+++ src/Controller/ContentElement/RowStartController.php
@@ -10,6 +10,7 @@ use Contao\CoreBundle\Controller\ContentElement\AbstractContentElementController
 use Contao\CoreBundle\ServiceAnnotation\ContentElement;
 use Contao\CoreBundle\Twig\FragmentTemplate;
 use Symfony\Component\HttpFoundation\Request;
+use ManiaxAtWork\ContaoGridBundle\GridClasses;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -17,8 +18,15 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class RowStartController extends AbstractContentElementController
 {
+    public function __construct(private readonly GridClasses $gridClasses)
+    {
+    }
+
     protected function getResponse(FragmentTemplate $template, ContentModel $model, Request $request): Response
     {
+        $rowClass = $this->gridClasses->getRowClass();
+        $template->rowClass = $rowClass;
+
         if ($this->container->get('contao.routing.scope_matcher')->isBackendRequest($request)) {
             $template = new BackendTemplate('be_wildcard');
 
